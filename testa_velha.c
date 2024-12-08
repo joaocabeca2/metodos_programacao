60c86dd0b6b40939ec9e7786446179e41dc5ef7f
joaocabeca2 <joaopedro.df12@gmail.com>
2024-12-08 00:57:41 -0300

    teste 1 - falhandols

---
+++ testa_velha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+//#include "velha.h"
+
+void testarVerificaJogoDaVelha() {   
+    
+}
+
+int main(void) {
+    testarVerificaJogoDaVelha();
+    return 0;
+
+}
