13578a5e35cc4ece67f802ea1e52e02f4c6a41ab
joaocabeca2 <joaopedro.df12@gmail.com>
2024-12-08 09:36:19 -0300

    Refatorando o código para verificar apenas se x venceu em uma linha e adicionando a verificação para a vitória de O

--- velha.c
+++ velha.c
@@ -16,7 +16,15 @@ int verificaJogoDaVelha(int matriz[3][3]) {
     for (int i = 0; i < 3; i++) {
         if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2]) {
             if (matriz[i][0] == 1) return 1; // X venceu
-            if (matriz[i][0] == 2) return 2; // O venceu
+            //if (matriz[i][0] == 2) return 2; // O venceu
+        }
+    }
+
+    // Verificar colunas
+    for (int j = 0; j < 3; j++) {
+        if (matriz[0][j] == matriz[1][j] && matriz[1][j] == matriz[2][j]) {
+            //if (matriz[0][j] == 1) return 1; // X venceu
+            if (matriz[0][j] == 2) return 2; // O venceu
         }
     }
 
