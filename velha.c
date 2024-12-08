6197fe69938f0e1230f1c9305ee88b83545f37c7
joaocabeca2 <joaopedro.df12@gmail.com>
2024-12-08 09:15:06 -0300

    Adicionando código para verificar se o tabluleiro está vazio e passando no teste 1

--- velha.c
+++ velha.c
@@ -2,5 +2,23 @@
 #include <stdio.h>
 // Implementação da função que verifica o estado do jogo da velha
 int verificaJogoDaVelha(int matriz[3][3]) {
-    
+    int x_count = 0, o_count = 0;
+
+    // Contar o número de X e O
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matriz[i][j] == 1) x_count++;
+            else if (matriz[i][j] == 2) o_count++;
+        }
+
+    // Verificar se o jogo está indefinido (há posições vazias)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matriz[i][j] == 0) return -1; // Jogo indefinido
+        }
+    }
+
+    // Se nenhum dos casos anteriores for verdadeiro, é empate
+    return 0;
 }
+
