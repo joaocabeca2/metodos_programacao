0dfba868277e82858f7aedff906acd44f2fec98e
joaocabeca2 <joaopedro.df12@gmail.com>
2024-12-08 09:50:13 -0300

    Teste 5 - Código para verificar um jogo impossível e falhando na primeira execução

--- testa_velha.c
+++ testa_velha.c
@@ -41,6 +41,15 @@ void testarVerificaJogoDaVelha() {
     assert(verificaJogoDaVelha(tabuleiro4) == 0);
     printf("Teste 4 (Empate): OK\n");
 
+    // Teste 5: Jogo impossível
+    int tabuleiro5[3][3] = {
+        {1, 1, 1},
+        {2, 2, 2},
+        {1, 2, 1}
+    };
+    assert(verificaJogoDaVelha(tabuleiro5) == -2);
+    printf("Teste 5 (Jogo impossível): OK\n");
+
 }
 
 int main() {
